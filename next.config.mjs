ad075b54d5bfb9ea0b7fe9185813f3520ff201f7
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-05-05 21:26:24 +0100

    problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true,
-    // swcMinify: true,
-    // assetPrefix: process.env.NODE_ENV === 'production' ? '/Niftables_Next/' : '',
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+      return [
+        {
+          source: '/public/videos/:path*',
+          destination: '/:path*',
+          permanent: true,
+        },
+      ];
+    },
   };
 
 export default nextConfig;
