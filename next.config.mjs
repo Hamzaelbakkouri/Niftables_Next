27266429b69e07080bc23d47a5d53a5e4eb46d47
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-05-05 21:12:55 +0100

    production videos link

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,16 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Niftables_Next/' : '',
     async redirects() {
-        return [
-            {
-                source: '/videos/:path*',
-                destination: '/:path*',
-                permanent: true,
-            },
-        ];
+      return [
+        {
+          source: '/videos/:path*',
+          destination: '/:path*',
+          permanent: true,
+        },
+      ];
     },
-};
+  };
 
 export default nextConfig;
