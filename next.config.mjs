befb1849891400343ae6567836e2d774f167edaa
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-05-05 21:18:52 +0100

    production videos link

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Niftables_Next/' : '',
-    async redirects() {
-      return [
-        {
-          source: '/videos/:path*',
-          destination: '/:path*',
-          permanent: true,
-        },
-      ];
-    },
+    // reactStrictMode: true,
+    // swcMinify: true,
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/Niftables_Next/' : '',
   };
 
 export default nextConfig;
